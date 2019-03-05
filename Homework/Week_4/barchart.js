17c4f74ddc2040f8295d9b8784f06689115aae79
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-03-05 21:32:03 +0100

    LT - verder barchart

--- Homework/Week_4/barchart.js
+++ Homework/Week_4/barchart.js
@@ -1,33 +1,23 @@
 // Name: Michael Stroet
 // Student number: 11293284
 
-d3.json("data.json").then(function(data) {
-  barChart(data)
+d3.json("games.json").then(function(dataset) {
+  barChart(dataset)
 });
 
-function barChart(data) {
+function barChart(dataset) {
+  /*
+  Draws an interactive barchart of the given data
+  */
 
+  // Dimensions of the figure
   var svgWidth = 700;
   var svgHeight = 500;
-  var padding = 60;
 
+  // Dimensions for the barchart with padding on all sides
+  var padding = 60;
   var chartWidth = svgWidth - 2 * padding
   var chartHeight = svgHeight - 2 * padding
-
-  const dataset = [
-    {
-      platform: "Windows",
-      games: 13000,
-    },
-    {
-      platform: "Mac",
-      games: 5000,
-    },
-    {
-      platform: "Linux",
-      games: 4500,
-    },
-  ];
 
   // Define a "svg" for drawing the figure
   const svg = d3.select("body")
@@ -43,85 +33,99 @@ function barChart(data) {
   // Scaling function for x values
   const xScale = d3.scaleBand()
     .range([0, chartWidth])
-    .domain(dataset.map((s) => s.platform))
+    .domain(dataset.map((data) => data.platform))
     .padding(0.2)
 
   // Scaling function for y values
   const yScale = d3.scaleLinear()
     .range([chartHeight, 0])
     .domain([0, 13360]);
+  /*
+  var tip = d3.tip()
+    .attr("class", "d3-tip")
+    .html(function(d) {
+      return "<strong>Frequency:</strong> <span style="color:red">" + d.games + "</span>";
+    });
+    */
 
   // Draw x-axis
   barChart.append("g").call(d3.axisBottom(xScale))
     .attr("class", "axis")
-    .attr('transform', `translate(0, ${chartHeight})`);
+    .attr("transform", `translate(0, ${chartHeight})`);
 
   // Draw x label
-  svg.append('text')
-    .attr('class', 'label')
-    .attr('x', chartWidth / 2 + padding)
-    .attr('y', chartHeight + padding * 1.7)
-    .attr('text-anchor', 'middle')
-    .text('Platforms')
+  svg.append("text")
+    .attr("class", "label")
+    .attr("x", chartWidth / 2 + padding)
+    .attr("y", chartHeight + padding * 1.7)
+    .attr("text-anchor", "middle")
+    .text("Platforms")
 
   // Draw y-axis
   barChart.append("g").call(d3.axisLeft(yScale))
     .attr("class", "axis");
 
   // Draw y label
-  svg
-    .append('text')
-    .attr('class', 'label')
-    .attr('x', -(chartHeight / 2) - padding)
-    .attr('y', padding / 3.5)
-    .attr('transform', 'rotate(270)')
-    .attr('text-anchor', 'middle')
-    .text('Number of games')
+  svg.append("text")
+    .attr("class", "label")
+    .attr("x", -(chartHeight / 2) - padding)
+    .attr("y", padding / 3.5)
+    .attr("transform", "rotate(270)")
+    .attr("text-anchor", "middle")
+    .text("Number of games")
 
   // Draw title
-  svg.append('text')
-    .attr('class', 'title')
-    .attr('x', chartWidth / 2 + padding)
-    .attr('y', 40)
-    .attr('text-anchor', 'middle')
-    .text('Total supported games on Steam per platform in 2016')
+  svg.append("text")
+    .attr("class", "title")
+    .attr("x", chartWidth / 2 + padding)
+    .attr("y", 40)
+    .attr("text-anchor", "middle")
+    .text("Total supported games on Steam per platform in 2016")
 
   // Draw horizontal gridlines
-  barChart.append('g')
-    .attr('class', 'grid')
+  barChart.append("g")
+    .attr("class", "grid")
     .attr("opacity", 0.3)
     .call(d3.axisLeft(yScale)
       .tickSize(-chartWidth, 0, 0)
-      .tickFormat('')
+      .tickFormat("")
     );
 
-  // Draw bars
+  //svg.call(tip);
+
+  // Define "g" for each bars
   var bars = barChart.selectAll(".bar")
   .data(dataset)
   .enter()
-  .append('g')
+  .append("g")
 
-  bars
-  .append('rect')
-  .attr('class', 'bar')
-  .attr('x', (g) => xScale(g.platform))
-  .attr('y', (g) => yScale(g.games))
-  .attr('height', (g) => chartHeight - yScale(g.games))
-  .attr('width', xScale.bandwidth())
+  // Draw bars
+  bars.append("rect")
+  .attr("class", "bar")
+  .attr("x", (data => xScale(data.platform)))
+  .attr("y", (data => yScale(data.games)))
+  .attr("height", (data => chartHeight - yScale(data.games)))
+  .attr("width", xScale.bandwidth())
+  /*
+  .on("mouseover", tip.show)
+  .on("mouseout", tip.hide)
+*/
+
+
   // Draw value of bar when mousing over it
-  .on('mouseenter', function (actual, i) {
+  .on("mouseover", function (d) {
   bars
-    .append('text')
-    .attr('class', 'values')
-    .attr('x', xScale(actual.platform) + xScale.bandwidth() / 2)
-    .attr('y', yScale(actual.games) + 30)
-    .attr('text-anchor', 'middle')
-    .text(actual.games)
+    .append("text")
+    .attr("class", "value")
+    .attr("x", xScale(d.platform) + xScale.bandwidth() / 2)
+    .attr("y", yScale(d.games) + 30)
+    .attr("text-anchor", "middle")
+    .text(d.games)
   })
 
-  // Remove value of bar when gone
-  .on('mouseleave', function () {
-  barChart.selectAll('.values').remove()
+  // Remove value of bar when the mouse is gone
+  .on("mouseout", function () {
+  barChart.selectAll(".value").remove()
   })
 
 };
