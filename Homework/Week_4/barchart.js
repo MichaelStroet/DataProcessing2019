689d241303c3ceeb70445aa528f192cb8892b218
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-03-07 15:06:19 +0100

    LT - verbeteringen in werkcollege vergeten te pushen

--- Homework/Week_4/barchart.js
+++ Homework/Week_4/barchart.js
@@ -17,12 +17,11 @@ function barChart(dataset) {
 
   // Dimensions for the barchart with padding on all sides
   var padding = 60;
-  var chartWidth = svgWidth - 2 * padding
-  var chartHeight = svgHeight - 2 * padding
+  var chartWidth = svgWidth - 2 * padding;
+  var chartHeight = svgHeight - 2 * padding;
 
   // Define a "svg" for drawing the figure
-  const svg = d3.select("body")
-    .append("svg")
+  const svg = d3.select("body").append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
@@ -31,17 +30,16 @@ function barChart(dataset) {
     .attr("class", "barchart")
     .attr("transform", `translate(${padding}, ${padding})`);
 
-    // Define the div for the tooltip
-    const div = d3.select("body")
-      .append('div')
-      .attr('class', 'tooltip')
-      .style('opacity', 0);
+  // Define the div for the tooltip
+  const div = d3.select("body").append('div')
+    .attr('class', 'tooltip')
+    .style('opacity', 0);
 
   // Scaling function for x values
   const xScale = d3.scaleBand()
     .range([0, chartWidth])
     .domain(dataset.map((data) => data.platform))
-    .padding(0.2)
+    .padding(0.2);
 
   // Scaling function for y values
   const yScale = d3.scaleLinear()
@@ -59,7 +57,7 @@ function barChart(dataset) {
     .attr("x", chartWidth / 2 + padding)
     .attr("y", chartHeight + padding * 1.7)
     .attr("text-anchor", "middle")
-    .text("Besturingssysteem")
+    .text("Besturingssysteem");
 
   // Draw y-axis
   barChart.append("g").call(d3.axisLeft(yScale))
@@ -68,11 +66,11 @@ function barChart(dataset) {
   // Draw y label
   svg.append("text")
     .attr("class", "label")
-    .attr("x", -(chartHeight / 2) - padding)
+    .attr("x", - (chartHeight / 2) - padding)
     .attr("y", padding / 3.5)
     .attr("transform", "rotate(270)")
     .attr("text-anchor", "middle")
-    .text("Aantal games")
+    .text("Aantal games");
 
   // Draw title
   svg.append("text")
@@ -80,7 +78,7 @@ function barChart(dataset) {
     .attr("x", chartWidth / 2 + padding)
     .attr("y", 40)
     .attr("text-anchor", "middle")
-    .text("Totaal aantal ondersteunde games per OS op Steam in 2017")
+    .text("Totaal aantal ondersteunde games per OS op Steam in 2017");
 
   // Draw horizontal gridlines
   barChart.append("g")
@@ -95,7 +93,7 @@ function barChart(dataset) {
   var bars = barChart.selectAll(".bar")
   .data(dataset)
   .enter()
-  .append("g")
+  .append("g");
 
   // Draw bars with tooltips of their value when mousing over them
   bars.append("rect")
