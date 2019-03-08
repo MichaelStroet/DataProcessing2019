ee526d7a60f257e745c07e75e1923d6f2c420522
Michael Stroet PC <michael.stroet@hotmail.com>
2019-03-08 13:39:34 +0100

    PC - scatterplot set up en laden van OECD data

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -1,2 +1,41 @@
 // Name: Michael Stroet
 // Student number: 11293284
+
+/*
+ * Main function
+ */
+
+window.onload = function() {
+
+    // Call OECD API for three datasets
+    const tourismInbound = "https://stats.oecd.org/SDMX-JSON/data/TOURISM_INBOUND/AUS+AUT+BEL+BEL-BRU+BEL-VLG+BEL-WAL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+EGY+MKD+IND+IDN+MLT+MAR+PER+PHL+ROU+RUS+ZAF.INB_ARRIVALS_TOTAL/all?startTime=2009&endTime=2017";
+    const purchasingPowerParities = "https://stats.oecd.org/SDMX-JSON/data/PPPGDP/PPP.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA18+OECD/all?startTime=2009&endTime=2017&dimensionAtObservation=allDimensions";
+    const grossDomesticProduct = "https://stats.oecd.org/SDMX-JSON/data/SNA_TABLE1/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA19+EU28+OECD+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+CYP+IND+IDN+MLT+ROU+RUS+SAU+ZAF.B1_GA.C/all?startTime=2009&endTime=2017&dimensionAtObservation=allDimensions";
+
+    // Convert the recieved responses to json strings
+    var requests = [d3.json(tourismInbound), d3.json(purchasingPowerParities), d3.json(grossDomesticProduct)];
+
+    // transform the responses into useful data objects
+    Promise.all(requests).then(function(response) {
+        let tourism = transformResponseTourism(response[0]);
+        let ppp = transformResponsePPP(response[1]);
+        let gdp = transformResponseGDP(response[2]);
+
+        // Draw a scatterplot with the recieved data
+        scatterPlot([tourism, ppp, gdp])
+
+        // Catch errors
+        }).catch(function(e){
+        throw(e);
+    });
+
+};
+
+
+/*
+ * Draws an interactive scatterplot
+ */
+
+function scatterPlot(datasets) {
+    console.log(datasets);
+};
---
+++ Homework/Week_5/transformResponses.js
@@ -0,0 +1,223 @@
+// Name: Michael Stroet
+// Student number: 11293284
+
+/********
+ * Transforms response of OECD request for inbound tourism.
+ * https://stats.oecd.org/SDMX-JSON/data/TOURISM_INBOUND/AUS+AUT+BEL+BEL-BRU+BEL-VLG+BEL-WAL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+EGY+MKD+IND+IDN+MLT+MAR+PER+PHL+ROU+RUS+ZAF.INB_ARRIVALS_TOTAL/all?startTime=2008&endTime=2017
+ **/
+
+function transformResponseTourism(data){
+
+    // Save data
+    let originalData = data;
+
+    // access data property of the response
+    let dataHere = data.dataSets[0].series;
+
+    // access variables in the response and save length for later
+    let series = data.structure.dimensions.series;
+    let seriesLength = series.length;
+
+    // set up array of variables and array of lengths
+    let varArray = [];
+    let lenArray = [];
+
+    series.forEach(function(serie){
+        varArray.push(serie);
+        lenArray.push(serie.values.length);
+    });
+
+    // get the time periods in the dataset
+    let observation = data.structure.dimensions.observation[0];
+
+    // add time periods to the variables, but since it's not included in the
+    // 0:0:0 format it's not included in the array of lengths
+    varArray.push(observation);
+
+    // create array with all possible combinations of the 0:0:0 format
+    let strings = Object.keys(dataHere);
+
+    // set up output object, an object with each country being a key and an array
+    // as value
+    let dataObject = {};
+
+    // for each string that we created
+    strings.forEach(function(string){
+        // for each observation and its index
+        observation.values.forEach(function(obs, index){
+            let data = dataHere[string].observations[index];
+            if (data != undefined){
+
+                // set up temporary object
+                let tempObj = {};
+
+                let tempString = string.split(":").slice(0, -1);
+                tempString.forEach(function(s, indexi){
+                    tempObj[varArray[indexi].name] = varArray[indexi].values[s].name;
+                });
+
+                // every datapoint has a time and ofcourse a datapoint
+                tempObj["Time"] = obs.name;
+                tempObj["Datapoint"] = data[0];
+                tempObj["Indicator"] = originalData.structure.dimensions.series[1].values[0].name;
+
+                // Add to total object
+                if (dataObject[tempObj["Country"]] == undefined){
+                  dataObject[tempObj["Country"]] = [tempObj];
+                } else {
+                  dataObject[tempObj["Country"]].push(tempObj);
+                };
+            }
+        });
+    });
+
+    // return the finished product!
+    return dataObject;
+};
+
+
+/********
+ * Transforms response of OECD request for purchasing parity power.
+ * https://stats.oecd.org/SDMX-JSON/data/PPPGDP/PPP.AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA18+OECD/all?startTime=2009&endTime=2017&dimensionAtObservation=allDimensions
+ **/
+
+function transformResponsePPP(data){
+
+    // Save data
+    let originalData = data;
+
+    // access data
+    let dataHere = data.dataSets[0].observations;
+
+    // access variables in the response and save length for later
+    let series = data.structure.dimensions.observation;
+    let seriesLength = series.length;
+
+    // get the time periods in the dataset
+    let observation = data.structure.dimensions.observation[0];
+
+    // set up array of variables and array of lengths
+    let varArray = [];
+    let lenArray = [];
+
+    series.forEach(function(serie){
+        varArray.push(serie);
+        lenArray.push(serie.values.length);
+    });
+
+    // add time periods to the variables, but since it's not included in the
+    // 0:0:0 format it's not included in the array of lengths
+    varArray.push(observation);
+
+    // create array with all possible combinations of the 0:0:0 format
+    let strings = Object.keys(dataHere);
+
+    // set up output array, an array of objects, each containing a single datapoint
+    // and the descriptors for that datapoint
+    let dataObject = {};
+
+    // for each string that we created
+    strings.forEach(function(string){
+        observation.values.forEach(function(obs, index){
+            let data = dataHere[string];
+            if (data != undefined){
+
+                // set up temporary object
+                let tempObj = {};
+
+                // split string into array of elements seperated by ':'
+                let tempString = string.split(":")
+                tempString.forEach(function(s, index){
+                    tempObj[varArray[index].name] = varArray[index].values[s].name;
+                });
+
+                tempObj["Datapoint"] = data[0];
+                tempObj["Indicator"] = obs.name;
+
+                // Add to total object
+                if (dataObject[tempObj["Country"]] == undefined){
+                  dataObject[tempObj["Country"]] = [tempObj];
+                } else {
+                  dataObject[tempObj["Country"]].push(tempObj);
+                };
+
+            }
+        });
+    });
+
+    // return the finished product!
+    return dataObject;
+};
+
+
+/********
+ * Transforms response of OECD request for GDP.
+ * https://stats.oecd.org/SDMX-JSON/data/SNA_TABLE1/AUS+AUT+BEL+CAN+CHL+CZE+DNK+EST+FIN+FRA+DEU+GRC+HUN+ISL+IRL+ISR+ITA+JPN+KOR+LVA+LTU+LUX+MEX+NLD+NZL+NOR+POL+PRT+SVK+SVN+ESP+SWE+CHE+TUR+GBR+USA+EA19+EU28+OECD+NMEC+ARG+BRA+BGR+CHN+COL+CRI+HRV+CYP+IND+IDN+MLT+ROU+RUS+SAU+ZAF.B1_GA.C/all?startTime=2012&endTime=2017&dimensionAtObservation=allDimensions
+ **/
+
+function transformResponseGDP(data){
+
+    // Save data
+    let originalData = data;
+
+    // access data
+    let dataHere = data.dataSets[0].observations;
+
+    // access variables in the response and save length for later
+    let series = data.structure.dimensions.observation;
+    let seriesLength = series.length;
+
+    // get the time periods in the dataset
+    let observation = data.structure.dimensions.observation[0];
+
+    // set up array of variables and array of lengths
+    let varArray = [];
+    let lenArray = [];
+
+    series.forEach(function(serie){
+        varArray.push(serie);
+        lenArray.push(serie.values.length);
+    });
+
+    // add time periods to the variables, but since it's not included in the
+    // 0:0:0 format it's not included in the array of lengths
+    varArray.push(observation);
+
+    // create array with all possible combinations of the 0:0:0 format
+    let strings = Object.keys(dataHere);
+
+    // set up output array, an array of objects, each containing a single datapoint
+    // and the descriptors for that datapoint
+    let dataObject = {};
+
+    // for each string that we created
+    strings.forEach(function(string){
+        observation.values.forEach(function(obs, index){
+            let data = dataHere[string];
+            if (data != undefined){
+
+                // set up temporary object
+                let tempObj = {};
+
+                // split string into array of elements seperated by ':'
+                let tempString = string.split(":")
+                tempString.forEach(function(s, index){
+                    tempObj[varArray[index].name] = varArray[index].values[s].name;
+                });
+
+                tempObj["Datapoint"] = data[0];
+
+                // Add to total object
+                if (dataObject[tempObj["Country"]] == undefined){
+                  dataObject[tempObj["Country"]] = [tempObj];
+                } else if (dataObject[tempObj["Country"]][dataObject[tempObj["Country"]].length - 1]["Year"] != tempObj["Year"]) {
+                    dataObject[tempObj["Country"]].push(tempObj);
+                };
+
+            }
+        });
+    });
+
+    // return the finished product!
+    return dataObject;
+};
