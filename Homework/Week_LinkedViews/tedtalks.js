01a1298e0940c15336a063ee9b45d7c0d0da8213
Michael Stroet Laptop <michael.stroet@hotmail.com>
2019-03-20 18:14:04 +0100

    LT - linked bar -> update calendar, nieuwe layout

--- Homework/Week_LinkedViews/tedtalks.js
+++ Homework/Week_LinkedViews/tedtalks.js
@@ -6,122 +6,161 @@ window.onload = function() {
      * Main function
      */
 
-     var inputJSON = "data.json";
-     var data = d3.json(inputJSON);
+    var inputJSON = "data.json";
+    var data = d3.json(inputJSON);
 
-     Promise.all([data]).then(function(response) {
+    Promise.all([data]).then(function(response) {
 
-         // Prepare the datasets for the scatterplot
-         var dataset = response[0];
+        //
+        var datasets = response[0];
 
-         barChart(dataset["barchart"]);
-         calendarView(dataset["calendar"]);
+        visualisationTedTalks(datasets);
 
-         // Catch errors
-         }).catch(function(e){
-             throw(e);
-     });
+        // Catch errors
+        }).catch(function(e){
+            throw(e);
+        });
+    };
+
+function visualisationTedTalks(datasets) {
+
+    var totalWidth = 1100,
+        totalHeight = 4000;
+
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", totalWidth)
+        .attr("height", totalHeight)
+        .attr("class", "visualisation");
+
+    var barWidth = totalWidth / 2,
+        barHeight = totalHeight,
+        calWidth = totalWidth - barWidth;
+
+    svg.append("g")
+        .attr("class", "gBarchart")
+        .attr("transform", `translate(0, 30)`);
+
+    // Define a "div" for the tooltip
+    d3.select("body")
+        .append('div')
+        .attr('class', 'barTooltip')
+        .style('opacity', 0);
+
+    svg.append("g")
+        .attr("class", "gCalendar")
+        .attr("transform", `translate(${barWidth}, 0)`);
+
+    // Define a "div" for the tooltip
+    d3.select("body")
+        .append('div')
+        .attr('class', 'calTooltip')
+        .style('opacity', 0);
+
+    var firstYear = 1984,
+        lastYear = 2018;
+
+    barChart(datasets, barWidth, barHeight, firstYear, lastYear);
+    enterCalendar(datasets["calendar"], calWidth, firstYear, lastYear);
+
 };
 
-function barChart(dataset) {
+function barChart(datasets, svgWidth, svgHeight, firstYear, lastYear) {
     /*
     Draws an interactive barchart of the given data
     */
 
-    // Dimensions of the figure
-    var svgWidth = 1280;
-    var svgHeight = 720;
-
     // Dimensions for the scatterplot with padding on all sides
     var padding = {
-        top: 30,
-        right: 20,
-        bottom: 50,
-        left: 100
+        top: 80,
+        right: 0,
+        bottom: 30,
+        left: 130
     };
 
     var chartWidth = svgWidth - padding.left - padding.right;
     var chartHeight = svgHeight - padding.top - padding.bottom;
 
-    var alphabeticalDataset = Object.entries(dataset).sort(function(a, b) {
+    var datasetBar = datasets["barchart"];
+
+    var alphabeticalDataset = Object.entries(datasetBar).sort(function(a, b) {
         a = a[0].toLowerCase();
         b = b[0].toLowerCase();
         return a < b ? -1 : a > b ? 1 : 0;
     });
 
-    var alphabeticalKeys = Object.keys(dataset).sort(function(a, b) {
+    var alphabeticalKeys = Object.keys(datasetBar).sort(function(a, b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
         return a < b ? -1 : a > b ? 1 : 0;
     });
 
+    // Select the "g" of the barchart
+    var g = d3.select(".gBarchart");
+
+    // Define a "div" for the tooltip
+    var tooltip = d3.select(".barTooltip");
+
     // Define a "svg" for drawing the figure
-    const svg = d3.select("body").append("svg")
+    var svg = g.append("svg")
         .attr("width", svgWidth)
         .attr("height", svgHeight);
 
     // Define a "g" for the barchart
-    const barChart = svg.append("g")
+    var barChart = svg.append("g")
         .attr("class", "barchart")
         .attr("transform", `translate(${padding.left}, ${padding.top})`);
 
-    // Define a "div" for the tooltip
-    const tooltip = d3.select("body").append('div')
-        .attr('class', 'tooltip')
-        .style('opacity', 0);
-
     // Scaling function for x values
-    const xScale = d3.scaleBand()
+    var xScale = d3.scaleLinear()
         .range([0, chartWidth])
-        .domain(alphabeticalKeys)
-        .padding(0.2);
+        .domain([0, d3.max(Object.keys(datasetBar).map((tag) => datasetBar[tag])) * 1.05]);
 
     // Scaling function for y values
-    const yScale = d3.scaleLinear()
-        .range([chartHeight, 0])
-        .domain([0, d3.max(Object.keys(dataset).map((tag) => dataset[tag])) * 1.02]);
+    var yScale = d3.scaleBand()
+        .range([0, chartHeight])
+        .domain(alphabeticalKeys)
 
     // Draw x-axis
-    barChart.append("g").call(d3.axisBottom(xScale))
-        .attr("class", "axis")
-        .attr("transform", `translate(0, ${chartHeight})`);
+    barChart.append("g").call(d3.axisTop(xScale))
+        .attr("class", "axis");
 
     // Draw x label
     svg.append("text")
         .attr("class", "label")
         .attr("x", chartWidth / 2 + padding.left)
-        .attr("y", chartHeight + padding.top + padding.bottom / 1.3)
+        .attr("y", padding.top / 1.5)
         .attr("text-anchor", "middle")
         .text("X label");
+
+    // Draw vertical gridlines
+    barChart.append("g")
+        .attr("class", "grid")
+        .attr("opacity", 0.3)
+        .call(d3.axisBottom(xScale)
+            .tickSize(chartHeight, 0, 0)
+            .tickFormat("")
+    );
 
     // Draw y-axis
     barChart.append("g").call(d3.axisLeft(yScale))
         .attr("class", "axis");
 
-    // Draw y label
-    svg.append("text")
-        .attr("class", "label")
-        .attr("x", - (chartHeight / 2) - padding.top)
-        .attr("y", padding.left / 5)
-        .attr("transform", "rotate(270)")
-        .attr("text-anchor", "middle")
-        .text("Y label");
-
-    // Draw horizontal gridlines
-    barChart.append("g")
-        .attr("class", "grid")
-        .attr("opacity", 0.3)
-        .call(d3.axisLeft(yScale)
-            .tickSize(-chartWidth, 0, 0)
-            .tickFormat("")
-        );
+    // // Draw y label
+    // svg.append("text")
+    //     .attr("class", "label")
+    //     .attr("x", - (chartHeight / 2) - padding.top)
+    //     .attr("y", padding.left / 5)
+    //     .attr("transform", "rotate(270)")
+    //     .attr("text-anchor", "middle")
+    //     .text("Y label");
 
     // Draw title
     svg.append("text")
         .attr("class", "title")
         .attr("id", "remove")
         .attr("x", chartWidth / 2 + padding.left)
-        .attr("y", padding.top / 2)
+        .attr("y", padding.top / 5)
         .attr("text-anchor", "middle")
         .text("Title");
 
@@ -134,15 +173,19 @@ function barChart(dataset) {
     bars.append("rect")
         .attr("class", "bar")
         .attr("x", function(data) {
-            return xScale(data[0]);
+            return 0;
         })
         .attr("y", function(data) {
-            return yScale(data[1]);
+            return yScale(data[0]);
         })
-        .attr("height", function(data) {
-            return chartHeight - yScale(data[1]);
+        .attr("height", yScale.bandwidth())
+        .attr("width", function(data) {
+            return xScale(data[1]);
         })
-        .attr("width", xScale.bandwidth())
+        .on("click", function(values) {
+            console.log(`Clicked on '${values[0]}', with ${values[1]} talks`);
+            return updateCalendar(datasets["calendar"], values[0], firstYear, lastYear);
+        })
         .on("mousemove", function(data) {
             tooltip
                 .transition()
@@ -162,80 +205,101 @@ function barChart(dataset) {
 
 };
 
-
-function calendarView(dataset) {
-    var defaultTag = "technology";
-
-    var dates = Object.keys(dataset[defaultTag]);
-    var years = [];
-
-    for (date in dates) {
-        years.push(dates[date].substring(0,4));
-    };
-    years = years.map(Number);
-
-    drawCalendar(dataset, d3.min(years), d3.max(years));
-};
-
-
-function drawCalendar(dataset, firstYear, lastYear) {
+// http://bl.ocks.org/GuilloOme/75f51c64c2132899d58d4cd6a23506d3
+function enterCalendar(dataset, svgWidth, firstYear, lastYear) {
     /*
     Draws an interactive calendar of the given data
     */
 
-    // http://bl.ocks.org/GuilloOme/75f51c64c2132899d58d4cd6a23506d3
+    // Define the default tag
+    var tag = "technology";
+
     var padding = {
-        left : 100,
-        right: 10,
-        top  : 20
+        left  : 60,
+        right : 20,
+        top   : 10,
+        bottom: 0
     };
 
     // Calculate the size of each calendar based on it's width
-    var yearWidth = 1280,
-        daysPerWeek = 7,
+    var daysPerWeek = 7,
         weeksPerYear = 53;
 
-    cellSize = (yearWidth - (padding.left + padding.right)) / weeksPerYear;
+    cellSize = (svgWidth - (padding.left + padding.right)) / weeksPerYear;
     var yearHeight = cellSize * daysPerWeek + padding.top;
 
-    var percent = d3.format(".1%");
     var format = d3.timeFormat("%Y-%m-%d");
 
-    // Create div for the calender view
-    d3.select("body").append("div")
-        .attr("class", "calendar")
+    // Select g of the calendar view
+    var g = d3.select(".gCalendar")
 
-    var svg = d3.select(".calendar").selectAll("svg")
-        .data(d3.range(firstYear, lastYear + 1))
+    // Select the tooltip div
+    var tooltip = d3.select(".calTooltip");
+
+    var svg = g.selectAll("svg")
+        .data(d3.range(firstYear, lastYear))
         .enter()
+        .append("g")
+        .attr("transform", function(data) {
+            console.log(data);
+            return `translate(0, ${(data - firstYear) * yearHeight + padding.top + padding.bottom})`;
+        })
         .append("svg")
-        .attr("width", yearWidth)
+        .attr("width", svgWidth)
         .attr("height", yearHeight)
-        .attr("class", "RdYlGn")
+        .attr("class", "calendarYear")
         .append("g")
         .attr("transform", `translate(${padding.left}, ${padding.top / 2})`);
 
     svg.append("text")
-        .attr("transform", `translate(${- cellSize}, ${(yearHeight - padding.top) / 2})rotate(270)`)
+        .attr("transform", `translate(${-2 * cellSize}, ${(yearHeight - padding.top) / 2})`)
         .style("text-anchor", "middle")
-        .text(function(data) {
-            return data;
+        .text(function(year) {
+            return year;
         });
 
     var rect = svg.selectAll(".day")
-        .data(function(data) { return d3.timeDays(new Date(data, 0, 1), new Date(data + 1, 0, 1)); })
+        .data(function(year) { return d3.timeDays(new Date(year, 0, 1), new Date(year + 1, 0, 1)); })
         .enter()
         .append("rect")
         .attr("class", "day")
         .attr("width", cellSize)
         .attr("height", cellSize)
-        .attr("x", function(data) { return d3.timeWeek.count(d3.timeYear(data), data) * cellSize; })
-        .attr("y", function(data) { return data.getDay() * cellSize; })
-        .datum(format);
+        .attr("x", function(date) { return d3.timeWeek.count(d3.timeYear(date), date) * cellSize; })
+        .attr("y", function(date) { return date.getDay() * cellSize; })
+        .attr("fill", function(date) {
+            if (dataset[tag][format(date)] == undefined) {
+                return "#fff";
+            };
+            if (dataset[tag][format(date)] == 1) {
+                return "yellow";
+            };
+            if (dataset[tag][format(date)] <= 5) {
+                return "orange";
+            };
+            if (dataset[tag][format(date)] <=10) {
+                return "red";
+            };
 
-    rect.append("title")
-        .text(function(data) {
-            return data;
+            return "darkred";
+
+        })
+        .datum(format)
+        .on("mousemove", function(date) {
+            tooltip
+                .transition()
+                .duration(50)
+                .style('opacity', 0.9);
+            tooltip
+                .html(date)
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 40) + "px");
+            })
+        .on("mouseout", function() {
+            tooltip
+                .transition()
+                .duration(500)
+                .style('opacity', 0);
         });
 
     svg.selectAll(".month")
@@ -247,7 +311,6 @@ function drawCalendar(dataset, firstYear, lastYear) {
         .attr("class", "month")
         .attr("d", monthPath);
 };
-
 
 function monthPath(t0) {
     /*
@@ -265,4 +328,31 @@ function monthPath(t0) {
         + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
         + "H" + (w1 + 1) * cellSize + "V" + 0
         + "H" + (w0 + 1) * cellSize + "Z";
+};
+
+function updateCalendar(dataset, newTag, firstYear, lastYear) {
+
+    var transDuration = 750;
+    var format = d3.timeFormat("%Y-%m-%d");
+
+    var svg = d3.select(".visualisation").transition();
+
+    var rect = svg.selectAll(".day")
+        .duration(transDuration)
+        .attr("fill", function(date) {
+            if (dataset[newTag][date] == undefined) {
+                return "#fff";
+            };
+            if (dataset[newTag][date] == 1) {
+                return "yellow";
+            };
+            if (dataset[newTag][date] <= 5) {
+                return "orange";
+            };
+            if (dataset[newTag][date] <=10) {
+                return "red";
+            };
+
+            return "darkred";
+        });
 };
